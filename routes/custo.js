f1dc99c3bfecda337f02cbe1211b4041e64c6073
Felipe Santos de Lima <103938043+Fslima2004@users.noreply.github.com>
2024-04-09 22:07:38 -0300

    Atualizar o custo.js

--- routes/custo.js
+++ routes/custo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('<h1>Calculo de custo...</h1>');
+  res.send('<h1>Turbo...</h1>');
 });
 
 module.exports = router;
