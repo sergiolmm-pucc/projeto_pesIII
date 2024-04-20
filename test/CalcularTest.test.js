048131264633169bc3cc6b73cd7f81e510590e92
Sergio Marques <sergio.marques@puc-campinas.edu.br>
2024-04-19 22:25:52 -0300

    api base2

--- test/CalcularTest.test.js
+++ test/CalcularTest.test.js
@@ -10,16 +10,8 @@ describe('Teste basico 1', ()=>{
         expect(calcular(2,3)).toBe(6);
     })
 
-
-})
-
-
-describe('Teste 2 ', ()=>{
-
-
     it('CalcularQuadrado de 9 ', ()=>{
         expect(calculaQuadrado(9)).toBe(81)
     })
-
 
 })
