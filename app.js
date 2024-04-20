9c558e3ecb0f711b309a64e60e18a879b48a6cce
Sergio Marques <sergio.marques@puc-campinas.edu.br>
2024-04-19 22:24:06 -0300

    api base2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rendaRouter = require('./routes/renda');
 var baseRouter  = require('./routes/base');
-var apiRouter  = require('./routes/api');
+var apiRouter   = require('./routes/api');
 
 var app = express();
 
--- test/CalcularTest.test.js
+++ test/CalcularTest.test.js
@@ -1,17 +1,18 @@
 
-const calcular = require('../public/javascripts/funcoes')
+const calcular = require('../public/javascripts/funcoes');
+const calculaQuadrado = require('../public/javascripts/f1');
 
 
 describe('Teste basico 1', ()=>{
 
 
     it('Valores corretos', ()=>{
-        expect(calcular(2,3)).toBe(6)
+        expect(calcular(2,3)).toBe(6);
     })
 
 
 })
-const calculaQuadrado = require('../public/javascripts/f1')
+
 
 describe('Teste 2 ', ()=>{
 
