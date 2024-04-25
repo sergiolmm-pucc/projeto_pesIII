4a173c88a08e7f79421660bfadae31e646021d51
Jose Durigon <jose.junior@puc-campinas.edu.br>
2024-04-25 16:32:44 -0300

    ...

--- routes/opcoes.js
+++ routes/opcoes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 
 
@@ -39,6 +40,19 @@ function cumulativeDistribution(x) {
   }
   return w;
 }
+
+
+// router.get('/', function(req, res, next) {
+//   fs.readFile('./public/html/base.html', function (err, html) {
+//       if (err) {
+//           throw err; 
+//       }      
+//       else { 
+//          res.writeHeader(200, {"Content-Type": "text/html"});  
+//           res.write(html);  
+//           res.end();  
+//       }
+//   });    
 
 /* GET options price. */
 router.get('/', function(req, res, next) {
--- test/opcoes.test.js
+++ test/opcoes.test.js
@@ -1,22 +1,29 @@
 // __tests__/optionsCalculator.test.js
 
-const calculateOptionPrice = require('../routes/opcoes').calculateOptionPrice;
+const calculateOptionPrice = require('../routes/opcoes')//.calculateOptionPrice;
+
 
 describe('Options Calculator', () => {
     it('should calculate the option price correctly for a call option', () => {
 
-        const stockPrice = 100;
-        const strikePrice = 90;
-        const timeToExpiration = 1;
-        const volatility = 0.2;
-        const riskFreeRate = 0.05;
+        // const stockPrice = 100;
+        // const strikePrice = 90;
+        // const timeToExpiration = 1;
+        // const volatility = 0.2;
+        // const riskFreeRate = 0.05;
+        // const optionType = 'call';
+
+        const stockPrice = 0;
+        const strikePrice = 0;
+        const timeToExpiration = 0;
+        const volatility = 0;
+        const riskFreeRate = 0;
         const optionType = 'call';
 
 
         const expectedOptionPrice = 14.656438634490996;
-
-
-        const optionPrice = calculateOptionPrice(stockPrice, strikePrice, timeToExpiration, volatility, riskFreeRate, optionType);
+        
+        var optionPrice = calculateOptionPrice(stockPrice, strikePrice, timeToExpiration, volatility, riskFreeRate, optionType);
 
 
         expect(optionPrice).toBeCloseTo(expectedOptionPrice, 4);
