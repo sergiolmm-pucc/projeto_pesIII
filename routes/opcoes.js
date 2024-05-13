6220a2785280bff27f985e3c6e497ed145556693
Vitor Justamand <justamandd@gmail.com>
2024-05-13 01:57:37 -0300

    chore: lint reclamando que o next do express não está sendo usado

--- routes/opcoes.js
+++ routes/opcoes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('<h1>PEDRINHO KKKKKK</h1>);
+router.get('/', function(req, res) {
+  res.send('<h1>PEDRINHO KKKKKK</h1>')
 });
 
 module.exports = router;
