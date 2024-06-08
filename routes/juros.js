8d14902b2152fd4647116a237cae671cd0c7eafa
Pedro Barboza <pedro,bv1@puccampinas.edu.br>
2024-06-07 22:41:21 -0300

    Juros Atualizado

--- routes/juros.js
+++ routes/juros.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('<h1>Calculo de juros...</h1>');
+router.get('/juros', function(req, res) {
+  res.sendFile(__dirname + "/../public/html/juros_index.html");
+});
+
+router.post('/calculateLoan', function(req, res) {
+  const loanAmount = parseFloat(req.body.loanAmount);
+  const loanTerm = parseInt(req.body.loanTerm);
+  const interestRate = parseFloat(req.body.interestRate);
+  const interestType = req.body.interestType;
+
+  let totalAmountPaid;
+  let totalInterestPaid;
+  let monthlyInterestRate;
+  let timeToRepay;
+
+  if (interestType === "monthly") {
+    monthlyInterestRate = interestRate / 100;
+    totalAmountPaid = loanAmount * Math.pow(1 + monthlyInterestRate, loanTerm);
+    totalInterestPaid = totalAmountPaid - loanAmount;
+    timeToRepay = loanTerm;
+  } else if (interestType === "yearly") {
+    monthlyInterestRate = interestRate / (12 * 100);
+    timeToRepay = loanTerm * 12;
+    totalAmountPaid = loanAmount * Math.pow(1 + monthlyInterestRate, timeToRepay);
+    totalInterestPaid = totalAmountPaid - loanAmount;
+  }
+
+  res.json({
+    timeToRepay: timeToRepay,
+    totalAmountPaid: totalAmountPaid.toFixed(2),
+    totalInterestPaid: totalInterestPaid.toFixed(2)
+  });
 });
 
 module.exports = router;
-/* teste push */
