b0861e15d1de9317adfafc89e23f077d486d9032
Jose Marchi <jose.durigo@outlook.com>
2024-06-08 19:59:31 -0300

    Teste

--- test/opcoes.test.js
+++ test/opcoes.test.js
@@ -3,21 +3,21 @@ const { calculateEuropeanOptionPrice, calculateAmericanOptionPrice } = require('
 describe('Option Pricing Calculations', () => {
   test('calculateEuropeanOptionPrice for call option', () => {
     const result = calculateEuropeanOptionPrice(100, 100, 1, 0.2, 0.05, 'call');
-    expect(result).toBeCloseTo(10.45, 2); // Adjust the expected value as necessary
+    expect(result).toBeCloseTo(10.45, 2); y
   });
 
   test('calculateEuropeanOptionPrice for put option', () => {
     const result = calculateEuropeanOptionPrice(100, 100, 1, 0.2, 0.05, 'put');
-    expect(result).toBeCloseTo(5.57, 2); // Adjust the expected value as necessary
+    expect(result).toBeCloseTo(5.57, 2); 
   });
 
   test('calculateAmericanOptionPrice for call option', () => {
     const result = calculateAmericanOptionPrice(100, 100, 1, 0.2, 0.05, 'call');
-    expect(result).toBeCloseTo(10.45, 2); // Adjust the expected value as necessary
+    expect(result).toBeCloseTo(10.45, 2); 
   });
 
   test('calculateAmericanOptionPrice for put option', () => {
     const result = calculateAmericanOptionPrice(100, 100, 1, 0.2, 0.05, 'put');
-    expect(result).toBeCloseTo(6.08, 1); // Adjust the expected value as necessary
+    expect(result).toBeCloseTo(6.08, 1); 
   });
 });
