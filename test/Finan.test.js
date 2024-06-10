69cd1152199656b8cb952ca19155c4dd7ba325bf
Matheus Miura <matheus.miura@luminiitsolutions.com>
2024-06-10 17:31:25 -0300

    testando2

--- test/Finan.test.js
+++ test/Finan.test.js
@@ -1,7 +1,23 @@
-const {installment} = require('../public/javascripts/finan')
+const { installment } = require('../public/javascripts/finan');
 
-describe('Teste financiamento 1', ()=>{
-    test('Financiamento imobiliario', ()=>{
-        expect(installment(100000, 30000, 36, 'Casa')).toBe(1954)
-    })
-})
+describe('Teste financiamento 1', () => {
+
+  beforeAll(() => {
+    document.body.innerHTML = `
+      <form id="form">
+        <input type="text" id="propertyValue" value="30000">
+        <input type="text" id="valueImovel" value="100000">
+        <input type="text" id="term" value="36">
+        <select id="typeImovel">
+          <option value="Casa" selected>Casa</option>
+        </select>
+        <div id="result_final"></div>
+      </form>
+    `;
+  });
+
+  test('Financiamento imobiliario', () => {
+    const result = installment(100000, 30000, 36, 'Casa');
+    expect(result).toBeCloseTo(1954, 0);
+  });
+});
