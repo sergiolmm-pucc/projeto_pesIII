b038f6e931ec0591373410c184c238419c26084d
Matheus Miura <matheus.miura@luminiitsolutions.com>
2024-06-10 17:46:56 -0300

    testando6

--- test/Finan.test.js
+++ test/Finan.test.js
@@ -1,7 +1,8 @@
-const {installment} = require('../public/javascripts/finan')
+const {callInstallment} = require('../public/javascripts/finan');
+const {describe, test, expect} = require('@jest/globals')
 
 describe('Teste financiamento 1', ()=>{
     test('Financiamento imobiliario', ()=>{
-        expect(installment(100000, 30000, 36, 'Casa')).toBe(1954)
+        expect(callInstallment(100000, 30000, 36, 'Casa')).toBe(1954)
     })
 })
