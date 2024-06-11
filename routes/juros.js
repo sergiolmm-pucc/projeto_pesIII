7205c2083a9e2c506c606948369b997f3e18f941
RenatoWilkerPuc <renato.wps@puccampinas.edu.br>
2024-06-11 15:07:39 -0300

    update juros

--- routes/juros.js
+++ routes/juros.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/juros', function(req, res) {
+router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../views/juros_index.html'));
 });
 
