74af40341e9838f7738e820872edbd266856e99c
RenatoWilkerPuc <renato.wps@puccampinas.edu.br>
2024-06-11 16:04:48 -0300

    update juros

--- public/javascripts/juros_script.js
+++ public/javascripts/juros_script.js
@@ -1,3 +1,35 @@
+function calculateLoan() {
+    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
+    const loanTerm = parseInt(document.getElementById('loanTerm').value);
+    const interestRate = parseFloat(document.getElementById('interestRate').value);
+    const interestType = document.getElementById('interestType').value;
+
+    let totalAmountPaid;
+    let totalInterestPaid;
+    let monthlyInterestRate;
+    let timeToRepay;
+
+    if (interestType === "monthly") {
+        monthlyInterestRate = interestRate / 100;
+        totalAmountPaid = loanAmount * Math.pow(1 + monthlyInterestRate, loanTerm);
+        totalInterestPaid = totalAmountPaid - loanAmount;
+        timeToRepay = loanTerm;
+    } else if (interestType === "yearly") {
+        monthlyInterestRate = interestRate / (12 * 100);
+        timeToRepay = loanTerm * 12;
+        totalAmountPaid = loanAmount * Math.pow(1 + monthlyInterestRate, timeToRepay);
+        totalInterestPaid = totalAmountPaid - loanAmount;
+    }
+
+    document.getElementById('timeToRepay').innerText = timeToRepay + ' meses';
+    document.getElementById('totalAmountPaid').innerText = totalAmountPaid.toFixed(2);
+    document.getElementById('totalInterestPaid').innerText = totalInterestPaid.toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+}
+
+
+/*
 document.getElementById('loanForm').addEventListener('submit', function(event) {
     event.preventDefault();
     calculateLoan();
@@ -18,3 +50,4 @@ function calculateLoan() {
     })
     .catch(error => console.error('Error:', error));
 }
+*/
