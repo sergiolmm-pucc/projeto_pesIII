cf86a5ac0ee591a6dda085f40c5f63a38d7ad21d
sergiolmm-pucc <104084417+sergiolmm-pucc@users.noreply.github.com>
2024-06-14 14:23:16 -0300

    Update cambio.js

--- routes/cambio.js
+++ routes/cambio.js
@@ -1,19 +1,23 @@
-fs.readFile('./public/html/Home.html', function (err, html) {
+var fs = require('fs');
 
-      if (err) {
 
-          throw err;
+var express = require('express');
+var router = express.Router();
 
-      }     
+/* GET home page. */
+router.get('/', function(req, res) {
+    fs.readFile('./public/html/Home.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }      
+        else { 
+           res.writeHeader(200, {"Content-Type": "text/html"});  
+            res.write(html);  
+            res.end();  
+        }
+    });    
+      
 
-      else {
+});
 
-         res.writeHeader(200, {"Content-Type": "text/html"}); 
-
-          res.write(html); 
-
-          res.end(); 
-
-      }
-
-  }); 
+module.exports = router;
